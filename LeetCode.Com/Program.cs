474b4f73c40e830ad58aa3c85498084f69db4888
akalin <gargoyle_shadow@qq.com>
2019-03-23 14:25:57 +0800

    begin zero

---
+++ LeetCode.Com/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+
+
+            Console.ReadKey();
+        }
+    }
+}
