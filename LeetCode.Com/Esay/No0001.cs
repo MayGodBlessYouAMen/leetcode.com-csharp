406bff06e8d0b7e395243d34c3adf16deb577abc
akalin <gargoyle_shadow@qq.com>
2019-03-23 14:42:25 +0800

    No0001

---
+++ LeetCode.Com/Esay/No0001.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Esay
+{
+    /// <summary>
+    /// Two Sum
+    /// </summary>
+    public class No0001
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            //sort
+            int[] nums1 = Sort(nums);
+            //find
+            int start = 0;
+            int end = nums.Length - 1;
+            while (start < end)
+            {
+                if (nums1[start] + nums1[end] == target)
+                {
+                    break;
+                }
+                else if (nums1[start] + nums1[end] < target)
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+            return new int[] { GetStartIndex(nums, nums1[start]), GetEndIndex(nums, nums1[end]) };
+        }
+
+        public int[] Sort(int[] nums)
+        {
+            int[] numsCopy = new int[nums.Length];
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                numsCopy[i] = nums[i];
+            }
+            for (int i = 0; i < numsCopy.Length - 1; i++)
+            {
+                for (int j = 0; j < numsCopy.Length - 1 - i; j++)
+                {
+                    if (numsCopy[j] > numsCopy[j + 1])
+                    {
+                        int temp = numsCopy[j];
+                        numsCopy[j] = numsCopy[j + 1];
+                        numsCopy[j + 1] = temp;
+                    }
+                }
+            }
+            return numsCopy;
+        }
+
+        public int GetStartIndex(int[] nums, int num)
+        {
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                if (nums[i] == num)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int GetEndIndex(int[] nums, int num)
+        {
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] == num)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- LeetCode.Com/Program.cs
+++ LeetCode.Com/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetCode.Com.Esay;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +8,19 @@ namespace LeetCode.Com
 {
     class Program
     {
+        /// <summary>
+        /// 测试类
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
 
+            #region No0001
+            //No0001 no0001 = new No0001();
+            //int[] a = {2, 7, 11, 15};
+            //int[] b = no0001.TwoSum(a, 9);
+            #endregion
 
             Console.ReadKey();
         }
