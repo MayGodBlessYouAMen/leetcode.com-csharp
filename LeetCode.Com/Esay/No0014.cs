ad9a97d6125db421648ff55338b28b69a6cce04c
akalin <gargoyle_shadow@qq.com>
2019-03-27 09:24:52 +0800

    No0014

---
+++ LeetCode.Com/Esay/No0014.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Esay
+{
+    /// <summary>
+    /// Longest Common Prefix
+    /// </summary>
+    public class No0014
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+            if (strs.Length == 1) return strs[0];
+
+            return Compare(strs, strs[strs.Length - 1], strs.Length - 2);
+        }
+
+        public string Compare(string[] strs, string str, int n)
+        {
+            if (n < 0) return "";
+
+            if (n == 0)
+            {
+                return Compare(str, strs[0]);
+            }
+
+            string temp = Compare(str, strs[n]);
+            if (temp == "")
+            {
+                return "";
+            }
+            return Compare(strs, temp, n - 1);
+        }
+
+        public string Compare(string str1, string str2)
+        {
+            int minlength = str1.Length > str2.Length ? str2.Length : str1.Length;
+            string temp = "";
+            for (int i = 0; i < minlength; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    break;
+                }
+
+                temp += str1[i];
+            }
+            return temp;
+        }
+    }
+}
