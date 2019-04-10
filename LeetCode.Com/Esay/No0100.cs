50707ff51b0a611f715beaa6658bb50810046fa6
akalin <gargoyle_shadow@qq.com>
2019-04-10 23:06:59 +0800

    No0100

---
+++ LeetCode.Com/Esay/No0100.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Com.Com;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Esay
+{
+    /// <summary>
+    /// Same Tree
+    /// </summary>
+    public class No0100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+
+            //递归
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            else if (p == null || q == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (p.val == q.val)
+                {
+                    return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+    }
+}
