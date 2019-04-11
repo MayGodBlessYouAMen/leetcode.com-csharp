a893e88c74f402e3712209473284251c67b0035a
akalin <gargoyle_shadow@qq.com>
2019-04-11 08:49:10 +0800

    No0104

---
+++ LeetCode.Com/Com/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Com
+{
+    //Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
---
+++ LeetCode.Com/Esay/No0104.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Com.Com;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Esay
+{
+    /// <summary>
+    /// Maximum Depth of Binary Tree
+    /// </summary>
+    public class No0104
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            int leftDepth = 1;
+            int rightDepth = 1;
+
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.left != null)
+            {
+                leftDepth = MaxDepth(root.left) + 1;
+            }
+
+            if (root.right != null)
+            {
+                rightDepth = MaxDepth(root.right) + 1;
+            }
+
+            return leftDepth >= rightDepth ? leftDepth : rightDepth;
+        }
+    }
+}
