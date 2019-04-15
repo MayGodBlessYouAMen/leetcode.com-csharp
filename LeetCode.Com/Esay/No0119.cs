22028b9cc6af567d44c7ca314b612fdf46bb69fa
akalin <gargoyle_shadow@qq.com>
2019-04-15 11:56:59 +0800

    No0119

---
+++ LeetCode.Com/Esay/No0119.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Esay
+{
+    /// <summary>
+    /// Pascal's Triangle II  
+    /// </summary>
+    public class No0119
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            /*杨辉三角 第n行*/
+            IList<int> listi = new List<int>();
+
+            for (int i = 0; i <= rowIndex; i++)  //注意此题rowIndex是从0开始的
+            {
+                listi.Add(1);  //末尾添加1
+
+                for (int j = i; j >= 0; j--)   //倒序，a(n) = a(n) + a(n-1)
+                {
+                    //去头去尾
+                    if (j == i || j == 0)
+                    {
+                        continue;
+                    }
+
+                    //中间
+                    listi[j] = listi[j] + listi[j - 1];
+                }
+            }
+
+            return listi;
+        }
+    }
+}
