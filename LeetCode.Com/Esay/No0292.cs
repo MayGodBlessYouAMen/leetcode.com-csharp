35caee3fec9aba948b784c8d30dda26e254be2d7
akalin <gargoyle_shadow@qq.com>
2019-06-02 22:13:12 +0800

    No0292

---
+++ LeetCode.Com/Esay/No0292.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Esay
+{
+    /// <summary>
+    /// Nim Game
+    /// </summary>
+    public class No0292
+    {
+        public bool CanWinNim(int n)
+        {
+            //n=4的时候，先拿的人必输;推理可知n为4的倍数时也是必输;
+            //n!=4时，保证自己每次拿了之后剩余的为4的倍数即可胜利
+            if (n % 4 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
