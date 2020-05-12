524aca8d3a353367781d69a7654baad3c0d596e4
akalin <gargoyle_shadow@qq.com>
2020-05-12 18:03:47 +0800

    No0107

---
+++ LeetCode.Com/Esay/No0107.cs
@@ -0,0 +1,59 @@
+﻿using LeetCode.Com.Com;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Esay
+{
+    /// <summary>
+    /// Binary Tree Level Order Traversal II
+    /// </summary>
+    public class No0107
+    {
+        Queue<TreeNode> queue1 = new Queue<TreeNode>();
+        Queue<TreeNode> queue2 = new Queue<TreeNode>();
+
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            if (root == null)
+            {
+                return list;
+            }
+            //广度优先遍历，用队列存储下一层的节点
+            Queue<TreeNode> currentqueue = queue1;
+
+            currentqueue.Enqueue(root);
+            while (currentqueue.Count > 0)
+            {
+                IList<int> sublist = DoSome(ref currentqueue);
+                //倒序添加
+                list.Insert(0, sublist);
+            }
+
+            return list;
+        }
+
+        private IList<int> DoSome(ref Queue<TreeNode> currentqueue)
+        {
+            IList<int> sublist = new List<int>();
+            var tempqueue = currentqueue;
+            currentqueue = queue1.Count == 0 ? queue1 : queue2;
+            while (tempqueue.Count > 0)
+            {
+                var treenode = tempqueue.Dequeue();
+                sublist.Add(treenode.val);
+                if (treenode.left != null)
+                {
+                    currentqueue.Enqueue(treenode.left);
+                }
+                if (treenode.right != null)
+                {
+                    currentqueue.Enqueue(treenode.right);
+                }
+            }
+            return sublist;
+        }
+    }
+}
--- LeetCode.Com/Program.cs
+++ LeetCode.Com/Program.cs
@@ -188,19 +188,28 @@ namespace LeetCode.Com
             #endregion
 
             #region No0101
-            //undone: test case
-            No0101 no0111 = new No0101();
-            TreeNode treenode0101_1 = Utils.CreateTreeNode(new List<int>() { 1, 2, 2, 3, 4, 4, 3 });
-            TreeNode treenode0101_2 = Utils.CreateTreeNode(new List<int>() { 1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5 });
-            TreeNode treenode0101_3 = Utils.CreateTreeNode(new List<string>() { "1", "2", "2", "null", "3", "null", "3" });
-            Console.WriteLine(no0111.IsSymmetric(treenode0101_1));
-            Console.WriteLine(no0111.IsSymmetric(treenode0101_2));
-            Console.WriteLine(no0111.IsSymmetric(treenode0101_3));
+            //No0101 no0101 = new No0101();
+            //TreeNode treenode0101_1 = Utils.CreateTreeNode(new List<int>() { 1, 2, 2, 3, 4, 4, 3 });
+            //TreeNode treenode0101_2 = Utils.CreateTreeNode(new List<int>() { 1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5 });
+            //TreeNode treenode0101_3 = Utils.CreateTreeNode(new List<string>() { "1", "2", "2", "null", "3", "null", "3" });
+            //Console.WriteLine(no0101.IsSymmetric(treenode0101_1));
+            //Console.WriteLine(no0101.IsSymmetric(treenode0101_2));
+            //Console.WriteLine(no0101.IsSymmetric(treenode0101_3));
             #endregion
 
             #region No0104
             //undone: test case
             No0104 no0114 = new No0104();
+            #endregion
+
+            #region No0107
+            //No0107 no0107 = new No0107();
+            //TreeNode treenode0107_1 = Utils.CreateTreeNode(new List<int>() { 1, 2, 2, 3, 4, 4, 3 });
+            //TreeNode treenode0107_2 = Utils.CreateTreeNode(new List<int>() { 1, 2, 2, 3, 4, 4, 3, 5, 6, 7, 8, 8, 7, 6, 5 });
+            //TreeNode treenode0107_3 = Utils.CreateTreeNode(new List<string>() { "3", "9", "20", "null", "null", "15", "7" });
+            //Utils.ConsoleWriteLine(no0107.LevelOrderBottom(treenode0107_1));
+            //Utils.ConsoleWriteLine(no0107.LevelOrderBottom(treenode0107_2));
+            //Utils.ConsoleWriteLine(no0107.LevelOrderBottom(treenode0107_3));
             #endregion
 
             #region No0118
