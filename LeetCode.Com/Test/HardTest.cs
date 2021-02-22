30a6742c515437a1ec9758b52055ed8f18e782c8
akalin <gargoyle_shadow@qq.com>
2021-02-22 09:46:00 +0800

    test

--- LeetCode.Com/Test/HardTest.cs
+++ LeetCode.Com/Test/HardTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//using LeetCode.Com.Hard;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,20 @@ namespace LeetCode.Com.Test
     public class HardTest
     {
         public static void Start()
-        { 
-            
+        {
+            #region No0010
+            //No0010 no0010 = new No0010();
+            //Console.WriteLine(no0010.IsMatch("a", "a") == true);
+            //Console.WriteLine(no0010.IsMatch("a", ".") == true);
+            //Console.WriteLine(no0010.IsMatch("aa", "a") == false);
+            //Console.WriteLine(no0010.IsMatch("aa", "a*") == true);
+            //Console.WriteLine(no0010.IsMatch("ab", ".*c") == false);
+            //Console.WriteLine(no0010.IsMatch("aab", "c*a*b*") == true);
+            //Console.WriteLine(no0010.IsMatch("mississippi", "mis*is*p*.") == false);
+            //Console.WriteLine(no0010.IsMatch("ab", ".*") == true);
+            //Console.WriteLine(no0010.IsMatch("aaa", "aaaa") == false);
+            //Console.WriteLine(no0010.IsMatch("aaa", "a*a") == true);
+            #endregion
         }
     }
 }
