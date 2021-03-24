efb781bf8de351e3d548d7d5ad37f40bd01cb4f9
akalin <gargoyle_shadow@qq.com>
2021-03-24 20:20:27 +0800

    No0461

---
+++ LeetCode.Com/Easy/No0461.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Easy
+{
+    /// <summary>
+    /// [461]  Hamming Distance
+    /// </summary>
+    public class No0461
+    {
+        public int HammingDistance(int x, int y)
+        {
+            int[] arr1 = ChangeIntToBinary(x);
+            int[] arr2 = ChangeIntToBinary(y);
+
+            int count = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private int[] ChangeIntToBinary(int num)
+        {
+            int n = num;
+            int[] arr = new int[32];
+            int maxindex = 0;
+            while (true)
+            {
+                arr[maxindex] = n % 2;
+                n = n / 2;
+                if (n == 0)
+                {
+                    break;
+                }
+                maxindex++;
+            }
+            return arr;
+        }
+    }
+}
