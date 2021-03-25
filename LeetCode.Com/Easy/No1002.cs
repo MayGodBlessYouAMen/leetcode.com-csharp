891452ecddafba25c888f7ff3907865fee31cb5b
akalin <gargoyle_shadow@qq.com>
2021-03-25 14:05:51 +0800

    add No1002 Easy

---
+++ LeetCode.Com/Easy/No1002.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Easy
+{
+    /// <summary>
+    /// [1002]  Find Common Characters
+    /// </summary>
+    public class No1002
+    {
+        public IList<string> CommonChars(string[] A)
+        {
+            IList<string> list = new List<string>();
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                string temp = ((char)i).ToString();
+                int count = A[0].Count(x => x == i);
+                for (int j = 0; j < A.Length; j++)
+                {
+                    //判断是否存在
+                    if (!A[j].Contains(temp))
+                    {
+                        count = 0;
+                        break;
+                    }
+
+                    //计算个数
+                    int countj = A[j].Count(x => x == i);
+                    if (countj < count)
+                    {
+                        count = countj;
+                    }
+                }
+                while (count > 0)
+                {
+                    list.Add(temp);
+                    count--;
+                }
+            }
+            return list;
+        }
+    }
+}
--- LeetCode.Com/Test/EasyTest.cs
+++ LeetCode.Com/Test/EasyTest.cs
@@ -914,6 +914,12 @@ namespace LeetCode.Com.Test
             //Console.WriteLine(no0997.FindJudge(4, new int[][] { new int[] { 1, 3 }, new int[] { 1, 4 }, new int[] { 2, 3 }, new int[] { 2, 4 }, new int[] { 4, 3 } }));
             #endregion
 
+            #region No1002
+            //No1002 no1002 = new No1002();
+            //Console.WriteLine(no1002.CommonChars(new string[] { "bella", "label", "roller" }).ToConsoleString());
+            //Console.WriteLine(no1002.CommonChars(new string[] { "cool", "lock", "cook" }).ToConsoleString());
+            #endregion
+
             #region No1141
             //No1141 no1141 = new No1141();
             //Console.WriteLine(no1141.BuildArray(new int[] { 1, 3 }, 3).ToConsoleString());
