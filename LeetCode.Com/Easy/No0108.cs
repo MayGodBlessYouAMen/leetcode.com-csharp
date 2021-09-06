77afd312f5a3ae4fdd15dab1247c0cb63dc86aca
akalin <gargoyle_shadow@qq.com>
2021-09-06 16:52:32 +0800

    no0108 easy

---
+++ LeetCode.Com/Easy/No0108.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Com.Com;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Easy
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+    public class No0108
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            if (nums == null || nums.Length <= 0)
+            {
+                return null;
+            }
+
+            int midIndex = nums.Length / 2;
+
+            int val = nums[midIndex];
+            var arrleft = nums.Skip(0).Take(midIndex).ToArray();
+            var arrright = nums.Skip(midIndex + 1).ToArray();
+
+            var left = SortedArrayToBST(arrleft);
+            var right = SortedArrayToBST(arrright);
+
+            TreeNode treeNode = new TreeNode(val, left, right);
+
+            return treeNode;
+
+        }
+    }
+}
