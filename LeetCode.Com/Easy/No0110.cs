500caa723979a3687fd6ac15218632b4d3661f6d
akalin <gargoyle_shadow@qq.com>
2021-09-06 17:21:54 +0800

    No0110 easy

---
+++ LeetCode.Com/Easy/No0110.cs
@@ -0,0 +1,65 @@
+﻿using LeetCode.Com.Com;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Easy
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+    public class No0110
+    {
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            int leftDepth = Depth(root.left);
+            int rigthDepth = Depth(root.right);
+
+            if (Math.Abs(leftDepth - rigthDepth) <= 1 &&
+                IsBalanced(root.left) &&
+                IsBalanced(root.right))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 计算树高度（递归）
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        private int Depth(TreeNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(Depth(node.left), Depth(node.right)) + 1;
+        }
+    }
+}
