5da74467ec3e2577f1365dc13ffa1492bf33d187
akalin <gargoyle_shadow@qq.com>
2021-09-07 11:30:02 +0800

    no0196 easy sql

---
+++ LeetCode.Com/Easy/No0196.sql
@@ -0,0 +1,13 @@
+﻿--mysql
+delete from person
+where id not in 
+(select id from (select MIN(Id) as id from person group by Email) as p)
+
+/*
+	注意mysql恶心的地方，下面写法会报错：
+	delete from person
+	where id not in 
+	(select MIN(Id) as id from person group by Email)
+
+	要给子查询写个别名，再查一遍。
+*/
