daad71461633d8f8cccc59f556337589e9070e8b
akalin <gargoyle_shadow@qq.com>
2022-03-10 10:30:28 +0800

    fix

--- LeetCode.Com/Test/EasyTest.cs
+++ LeetCode.Com/Test/EasyTest.cs
@@ -1224,15 +1224,15 @@ namespace LeetCode.Com.Test
             #endregion
 
             #region No0671
-            No0671 no0671 = new No0671();
-            {
-                var root = Utils.CreateTreeNode(new List<int?>() { 2, 2, 5, null, null, 5, 7 });
-                Console.WriteLine(no0671.FindSecondMinimumValue(root));
-            }
-            {
-                var root = Utils.CreateTreeNode(new List<int?>() { 2, 2, 2 });
-                Console.WriteLine(no0671.FindSecondMinimumValue(root));
-            }
+            //No0671 no0671 = new No0671();
+            //{
+            //    var root = Utils.CreateTreeNode(new List<int?>() { 2, 2, 5, null, null, 5, 7 });
+            //    Console.WriteLine(no0671.FindSecondMinimumValue(root));
+            //}
+            //{
+            //    var root = Utils.CreateTreeNode(new List<int?>() { 2, 2, 2 });
+            //    Console.WriteLine(no0671.FindSecondMinimumValue(root));
+            //}
             #endregion
 
             #region No0704
