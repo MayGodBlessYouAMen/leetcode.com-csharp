61172d3d8774c46d2c4fdb5a623b545e558ac40b
akalin <gargoyle_shadow@qq.com>
2022-04-04 11:19:51 +0800

    No0993 ac

---
+++ LeetCode.Com/Easy/No0993.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Com.Com;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Com.Easy
+{
+    /// <summary>
+    /// Cousins in Binary Tree
+    /// </summary>
+    public class No0993
+    {
+        /*
+         *   堂兄节点的定义：1.两个节点深度相同 2.两个节点父亲不同
+             解题思路：遍历树，记录找到的x和y的层深和父节点，最后做比较。
+                
+             注意：每个节点的值都不想等
+         */
+
+        TreeNode xParent;
+        TreeNode yParent;
+        int xDeep = -1;
+        int yDeep = -1;
+        public bool IsCousins(TreeNode root, int x, int y)
+        {
+            Traversal(root, x, y, null, 0);
+            return xDeep == yDeep && xParent != yParent;
+        }
+
+        private void Traversal(TreeNode root, int x, int y, TreeNode parent, int deep)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (root.val == x)
+            {
+                xParent = parent;
+                xDeep = deep;
+            }
+
+            if (root.val == y)
+            {
+                yParent = parent;
+                yDeep = deep;
+            }
+
+            //已经找到x和y的位置，结束递归
+            if (xDeep >= 0 && yDeep >= 0)
+            {
+                return;
+            }
+
+            Traversal(root.left, x, y, root, deep + 1);
+            Traversal(root.right, x, y, root, deep + 1);
+        }
+    }
+}
