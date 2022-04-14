96334165bc08e1fd6191aee9387dd062d844b7e4
akalin <gargoyle_shadow@qq.com>
2022-04-14 12:09:22 +0800

    No1084 sql ac

---
+++ LeetCode.Com/Easy/No1084.sql
@@ -0,0 +1,12 @@
+﻿--1084. Sales Analysis III
+--mysql
+--解题思路：用having代替子查询
+select p.product_id, p.product_name 
+from Product p 
+inner join Sales s on
+p.product_id = s.product_id
+group by p.product_id
+having
+min(s.sale_date)>='2019-01-01'
+and
+max(s.sale_date)<='2019-03-31'
