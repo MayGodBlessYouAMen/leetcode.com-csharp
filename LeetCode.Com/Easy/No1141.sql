e69f5aca737c3da657fb1af5a0cd7edcaf696180
akalin <gargoyle_shadow@qq.com>
2022-04-15 15:09:38 +0800

    no1141 sql ac

---
+++ LeetCode.Com/Easy/No1141.sql
@@ -0,0 +1,6 @@
+﻿-- 1141.User Activity for the Past 30 Days I
+-- Write your MySQL query statement below
+select activity_date day,count(distinct user_id) active_users
+from Activity
+where activity_date between '2019-06-28' and '2019-07-27'
+group by activity_date
