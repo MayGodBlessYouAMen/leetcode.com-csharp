0a5b3b39d2af8c77db69e512fdee58d39db47d4f
akalin <gargoyle_shadow@qq.com>
2022-04-19 12:08:11 +0800

    No1581 sql ac

---
+++ LeetCode.Com/Easy/No1581.sql
@@ -0,0 +1,13 @@
+﻿-- 1581.Customer Who Visited but Did Not Make Any Transactions
+-- Write your MySQL query statement below
+select
+customer_id,
+count(*) as count_no_trans
+from Visits
+where Visits.visit_id not in(
+    select distinct v.visit_id
+    from Visits v 
+        inner join Transactions t on
+        v.visit_id = t.visit_id
+)
+group by customer_id
