853f89d8d55c390bd43f5b2bf4bce988ae52d9f5
akalin <gargoyle_shadow@qq.com>
2022-04-19 11:47:35 +0800

    No1587 sql ac

---
+++ LeetCode.Com/Easy/No1587.sql
@@ -0,0 +1,9 @@
+﻿-- 1587.Bank Account Summary II
+-- Write your MySQL query statement below
+select 
+Users.name,
+sum(Transactions.amount) as balance
+from Users,Transactions
+where Users.account = Transactions.account
+group by Users.name
+Having balance > 10000
