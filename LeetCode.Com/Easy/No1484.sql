88e29891dc62d160a20fb70f359ab7c7c88b0c2f
akalin <gargoyle_shadow@qq.com>
2022-04-20 11:27:13 +0800

    No1484 sql ac

---
+++ LeetCode.Com/Easy/No1484.sql
@@ -0,0 +1,8 @@
+﻿--1484.Group Sold Products By The Date
+-- Write your MySQL query statement below
+select sell_date,
+count(distinct product) num_sold,
+group_concat(distinct product) products
+from Activities
+group by sell_date
+order by sell_date
