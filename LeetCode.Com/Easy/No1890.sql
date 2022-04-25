ffbf885470b3b09306e627f04c9d46d1c106bada
akalin <gargoyle_shadow@qq.com>
2022-04-25 13:23:51 +0800

    No1890 sql ac

---
+++ LeetCode.Com/Easy/No1890.sql
@@ -0,0 +1,6 @@
+﻿-- 1890.The Latest Login in 2020
+-- Write your MySQL query statement below
+select user_id, max(time_stamp) last_stamp
+from Logins
+where YEAR(time_stamp) = 2020
+group by user_id
