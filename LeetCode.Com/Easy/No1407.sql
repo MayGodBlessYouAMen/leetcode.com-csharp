7be85b82e6c1caf3de34ac1513365bbabc2c1700
akalin <gargoyle_shadow@qq.com>
2022-04-25 14:08:46 +0800

    No1407 sql ac

---
+++ LeetCode.Com/Easy/No1407.sql
@@ -0,0 +1,9 @@
+﻿-- 1407 Top Travellers
+-- Write your MySQL query statement below
+select Users.name,
+    ifnull(sum(Rides.distance),0) travelled_distance
+from Users
+ left join Rides on
+ Users.id = Rides.user_id
+ group by Users.name
+ order by travelled_distance desc,Users.name
