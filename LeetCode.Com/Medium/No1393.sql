60f050ded39f706406dabe4d39b1fd1649c9af0c
akalin <gargoyle_shadow@qq.com>
2022-04-29 11:23:12 +0800

    No1393 sql ac

---
+++ LeetCode.Com/Medium/No1393.sql
@@ -0,0 +1,8 @@
+﻿-- 1393.Capital Gain/Loss
+-- Write your MySQL query statement below
+select stock_name,
+sum(case when operation='Buy' then price*-1
+     else price
+     end) as capital_gain_loss
+from Stocks
+group by stock_name
