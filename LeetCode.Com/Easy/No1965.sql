ed567122597f04a97ad416e2433b430681e8b0d8
akalin <gargoyle_shadow@qq.com>
2022-05-06 09:43:56 +0800

    No1965 sql ac

---
+++ LeetCode.Com/Easy/No1965.sql
@@ -0,0 +1,19 @@
+﻿-- 1965.Employees With Missing Information
+-- Write your MySQL query statement below
+
+select t.employee_id
+from
+(
+    select
+    t1.employee_id
+    from Employees t1
+    where not exists(select t2.employee_id from Salaries t2 where t1.employee_id = t2.employee_id)
+
+    union
+
+    select
+    t1.employee_id
+    from Salaries t1
+    where not exists(select t2.employee_id from Employees t2 where t1.employee_id = t2.employee_id)
+) t
+order by t.employee_id
