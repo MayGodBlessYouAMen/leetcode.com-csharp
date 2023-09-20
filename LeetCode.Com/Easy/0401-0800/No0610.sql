a50659b9c9616010d39d4e0785eef052aace691c
akalin <gargoyle_shadow@qq.com>
2023-09-20 13:47:48 +0800

    No0610-->AC

---
+++ LeetCode.Com/Easy/0401-0800/No0610.sql
@@ -0,0 +1,15 @@
+﻿-- mysql
+-- 判断x、y、z能否构成三角形: 任意两边之和大于第三边
+select 
+	x,
+	y,
+	z,
+	(case 
+		when 
+			(x+y>z and x+z>y and y+z>x) 
+		then
+			'Yes'
+		else
+			'No'
+	end) as triangle
+from No0610_Triangle
