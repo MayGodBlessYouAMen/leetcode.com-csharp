755a44ade821419fbb9b57a08c57e9f6f56e35cf
akalin <gargoyle_shadow@qq.com>
2023-09-20 13:35:00 +0800

    No0607-->AC

---
+++ LeetCode.Com/Easy/0401-0800/No0607.sql
@@ -0,0 +1,13 @@
+﻿-- mysql
+select 
+	t1.name
+from No0607_SalesPerson t1
+where t1.sales_id not in(
+	select 
+	 t2.sales_id
+	from No0607_Orders t2
+	join No0607_Company t3 on 
+		t2.com_id = t3.com_id 
+		and
+		t3.name = 'RED' 
+)
